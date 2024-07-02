181dabb047e00fda3ef786b49fded1cf2cce4d8b
cremegon <hussainahmed.s.shaikh@gmail.com>
2024-07-02 15:49:28 +0100

    added contact page and color corrections

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,10 +5,31 @@ export default function Contact() {
     <div className="container">
       <Navbar />
       <div className="showcase-screen main">
-        <div className="art-content">
-          <h1 className="art-title">Art</h1>
+        <div className="contact-content">
+          <a
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=hussainahmed.s.shaikh@gmail.com
+"
+            target="_blank"
+          >
+            <div className="status-bar">
+              <div className="status-holder">
+                <img
+                  src="/src/assets/gmail-icon.png"
+                  alt="status-icon"
+                  className="status-icon"
+                />
+              </div>
+            </div>
+            <div className="contact-title-holder">
+              <img
+                src="/src/assets/finger3d.png"
+                alt="finger"
+                className="contact-ptr"
+              />
+              <h1 className="contact-title">get in touch</h1>
+            </div>
+          </a>
         </div>
-        <div className="showcase-bg layer"></div>
       </div>
     </div>
   );
