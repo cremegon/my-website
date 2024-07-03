bd4ed89dcdec01232327442be710210fbec675be
cremegon <hussainahmed.s.shaikh@gmail.com>
2024-07-03 17:07:20 +0100

    added the text content but not for wridden

--- src/pages/project-pages/ProjectNetflix.tsx
+++ src/pages/project-pages/ProjectNetflix.tsx
@@ -15,24 +15,39 @@ export default function Netflix() {
           <Carosel data={NetflixData} />
 
           <div className="text-content nier">
+            <h2>Objective:</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Egestas egestas fringilla phasellus faucibus scelerisque eleifend.
-              Laoreet id donec ultrices tincidunt. Tellus cras adipiscing enim
-              eu turpis egestas pretium. Pellentesque massa placerat duis
-              ultricies lacus sed. Aliquam ultrices sagittis orci a scelerisque
-              purus semper eget. Odio morbi quis commodo odio aenean sed
-              adipiscing. Eros in cursus turpis massa tincidunt dui ut ornare
-              lectus. Aliquam nulla facilisi cras fermentum odio. Lorem sed
-              risus ultricies tristique nulla aliquet enim. Congue quisque
-              egestas diam in arcu cursus euismod quis. Purus non enim praesent
-              elementum facilisis leo vel. Aenean euismod elementum nisi quis
-              eleifend quam adipiscing. Sit amet aliquam id diam maecenas. Sed
-              vulputate odio ut enim blandit volutpat maecenas volutpat. Massa
-              eget egestas purus viverra accumsan in nisl nisi. Enim praesent
-              elementum facilisis leo vel fringilla est.
+              In order to refine my understanding of the fundamental JavaScript
+              that the React framework is based on, I decided to challenge
+              myself by replicating the netflix landing page using only raw
+              JavaScript for functionality.
             </p>
+            <h2>Approach:</h2>
+            <p>
+              I decided to review concepts and the structure of the DOM, in
+              order to understand the underlying foundation upon which different
+              elements can be targeted. It was also to understand the DOM
+              history stack, that controls matters such as routing and data
+              sharing between pages.
+            </p>
+            <h2>Analysis:</h2>
+            <p>
+              The DOM structure is a tree of nodes that have their own
+              characteristics in the form of elements, by means of JavaScript
+              inbuilt functions to get elements and queries from the document,
+              we are able to manipulate the behavior of the site by manipulating
+              the elements within.
+            </p>
+            <h2>Outcome:</h2>
+            <p>
+              A successful replication of the Netflix homepage, that was is
+              identical to the original, as well as a newfound solid foundation
+              of how browsers, WEB APIS and the backend works.
+            </p>
+
+            <a href="/">
+              <div className="project-linked">visit the project here!</div>
+            </a>
           </div>
         </div>
       </div>
--- src/pages/project-pages/ProjectWridden.tsx
+++ src/pages/project-pages/ProjectWridden.tsx
@@ -15,6 +15,7 @@ export default function Wridden() {
           <Carosel data={WriddenData} />
 
           <div className="text-content nier">
+            <h2>Objective:</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -33,6 +34,41 @@ export default function Wridden() {
               eget egestas purus viverra accumsan in nisl nisi. Enim praesent
               elementum facilisis leo vel fringilla est.
             </p>
+            <h2>Research:</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Egestas egestas fringilla phasellus faucibus scelerisque eleifend.
+              Laoreet id donec ultrices tincidunt. Tellus cras adipiscing enim
+              eu turpis egestas pretium. Pellentesque massa placerat duis
+              ultricies lacus sed.
+            </p>
+            <h2>Approach:</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Egestas egestas fringilla phasellus faucibus scelerisque eleifend.
+              Laoreet id donec ultrices tincidunt. Tellus cras adipiscing enim
+              eu turpis egestas pretium. Pellentesque massa placerat duis
+              ultricies lacus sed. Aliquam ultrices sagittis orci a scelerisque
+              purus semper eget. Odio morbi quis commodo odio aenean sed
+              adipiscing. Eros in cursus turp
+            </p>
+            <h2>Outcome:</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Egestas egestas fringilla phasellus faucibus scelerisque eleifend.
+              Laoreet id donec ultrices tincidunt. Tellus cras adipiscing enim
+              eu turpis egestas pretium. Pellentesque massa placerat duis
+              ultricies lacus sed. Aliquam ultrices sagittis orci a scelerisque
+              purus semper eget. Odio morbi quis commodo odio aenean sed
+              adipiscing. Eros in cursus turp
+            </p>
+
+            <a href="/">
+              <div className="project-linked">visit the project here!</div>
+            </a>
           </div>
         </div>
       </div>
