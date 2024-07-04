ffb0cba036c7f71a7805861028eee22ed8a6d2aa
cremegon <hussainahmed.s.shaikh@gmail.com>
2024-07-04 23:17:37 +0100

    try:again

--- src/main.tsx
+++ src/main.tsx
@@ -11,39 +11,36 @@ import Contact from "./pages/Contact";
 import Netflix from "./pages/project-pages/ProjectNetflix";
 import Random from "./pages/project-pages/ProjectRandom";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/my-website",
-      element: <Home />,
-    },
-    {
-      path: "/project",
-      element: <Project />,
-    },
-    {
-      path: "/project/hero-website",
-      element: <Nier />,
-    },
-    {
-      path: "/project/wridden",
-      element: <Wridden />,
-    },
-    {
-      path: "/project/netflix-clone",
-      element: <Netflix />,
-    },
-    {
-      path: "/project/random",
-      element: <Random />,
-    },
-    {
-      path: "/contact",
-      element: <Contact />,
-    },
-  ],
-  { basename: "/my-website" }
-);
+const router = createBrowserRouter([
+  {
+    path: "/my-website",
+    element: <Home />,
+  },
+  {
+    path: "/project",
+    element: <Project />,
+  },
+  {
+    path: "/project/hero-website",
+    element: <Nier />,
+  },
+  {
+    path: "/project/wridden",
+    element: <Wridden />,
+  },
+  {
+    path: "/project/netflix-clone",
+    element: <Netflix />,
+  },
+  {
+    path: "/project/random",
+    element: <Random />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <style>
